d3adf177bcaa649f6b5a32b2a168e06e72ca0655
Amara Lovato <amaracuenca@gmail.com>
2017-01-09 14:36:15 -0700

    Adds file loader for multiple files working

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -2,12 +2,6 @@ require_relative 'test_helper'
 require './lib/enrollment_repository'
 
 class EnrollmentRepositoryTest < Minitest::Test
-  def setup
-    @er = EnrollmentRepository.new
-    @er.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"}})
-  end
 
   def test_it_has_a_class
     er = EnrollmentRepository.new
@@ -15,39 +9,50 @@ class EnrollmentRepositoryTest < Minitest::Test
   end
 
   def test_it_finds_district_when_known
-    setup
-    assert_equal Enrollment, @er.find_by_name("ACADEMY 20").class
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    assert_equal Enrollment, er.find_by_name("ACADEMY 20").class
   end
 
   def test_finder_is_case_insensitive
-    setup
-    assert_equal Enrollment, @er.find_by_name("academy 20").class
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    assert_equal Enrollment, er.find_by_name("academy 20").class
   end
 
   def test_it_gets_nil_if_district_unknown
-    setup
-    assert_nil @er.find_by_name("OHIO 216")
-  end
-
-  def test_it_adds_new_district_to_enrollment_data_tracker
     er = EnrollmentRepository.new
-    er.add_to_enrollments("MOFFAT 2", 2004, 0.01429)
-
-    assert_equal 1, er.enrollments.count
-    assert_equal Enrollment, er.add_to_enrollments("MOFFAT 2", 2004, 0.01429).class
+    er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    assert_nil er.find_by_name("OHIO 216")
   end
 
-  def test_it_adds_new_data_to_existant_district_in_data_tracker
-    er = EnrollmentRepository.new
-    er.add_to_enrollments("CENTENNIAL R-1", 2005, 0.10526)
-    er.add_to_enrollments("CENTENNIAL R-1", 2006, 04545)
-    expected = {:name=>"CENTENNIAL R-1", :kindergarten_participation=>{2005=>0.10526, 2006=>2405}}
+  # def test_it_adds_new_district_to_enrollment_data_tracker
+  # skip
+  # er = EnrollmentRepository.new
+  # er.add_to_enrollments("MOFFAT 2", 2004, 0.01429)
+  #
+  #   assert_equal 1, er.enrollments.count
+  #   assert_equal Enrollment, er.add_to_enrollments("MOFFAT 2", 2004, 0.01429).class
+  # end
 
-    assert_equal 1, er.enrollments.count
-    assert_equal expected, er.enrollments["CENTENNIAL R-1"].enrollment_data
-  end
+  # def test_it_adds_new_data_to_existant_district_in_data_tracker
+  #   skip
+  #   er = EnrollmentRepository.new
+  #   er.add_to_enrollments("CENTENNIAL R-1", 2005, 0.10526)
+  #   er.add_to_enrollments("CENTENNIAL R-1", 2006, 04545)
+  #   expected = {:name=>"CENTENNIAL R-1", :kindergarten_participation=>{2005=>0.10526, 2006=>2405}}
+  #
+  #   assert_equal 1, er.enrollments.count
+  #   assert_equal expected, er.enrollments["CENTENNIAL R-1"].enrollment_data
+  # end
 
-  def test_it_acces_name
+  def test_it_can_acces_name
     er = EnrollmentRepository.new
     er.load_data({
                    :enrollment => {
